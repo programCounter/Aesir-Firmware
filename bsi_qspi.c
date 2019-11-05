41c86669c12c51b258cd124757c47d206e8ef283
Landon B <landon.brown26@online.camosun.com>
2019-11-04 20:45:59 -0800

    Headers/Pages/Packets
    Trying them out, struct-ception

--- bsi_qspi.c
+++ bsi_qspi.c
@@ -16,6 +16,26 @@ bool lwrite_qspi = false;
 bool lread_qspi = false;
 bool m_finished = false; // used in the QSPI event
 
+BSI_Header TestHeader = {
+    .BSI_Name = {'T','E','S','T','\0'},
+    .StartTime = {20,19,11,4,19,9,0}, //   YY/YY/MM/DD/HH/MM/SS;
+    };
+
+// Empty header to read TestHeader back out of memory
+BSI_Header ReadHeader = {
+    };
+
+QSPI_Page PageTest = {
+    .countMin = 1440, //minutes since Header StartTime[] (last Local Listener connection)
+    .sensorCh = 1, //which sensor the following value is from (1=An1, 2=An2, or 9 = Pulse)
+    .sensorValue = 1023, //What reading did sensor take? (10b ADC or pulse)
+    };
+
+Ad_gPacket TestPacket = {
+//    .Header = TestHeader,
+//    .qPage = PageTest,
+};
+
 //Event called when the QSPI completes an action
 void qspi_handler(nrf_drv_qspi_evt_t event, void * p_context)
 {
@@ -55,65 +75,67 @@ void configure_memory()
 }
 
 //Code for reading and writing from QSPI, flag set in button event, method called in main
-void write_qspi()
+void write_qspi(uint32_t Address)
 {
     ret_code_t err_code;
-    volatile uint16_t nCount = pressCount;
-    volatile uint16_t c = 0;
+    //volatile uint16_t nCount = pressCount;
+    //volatile uint16_t c = 0;
     // counter for binary array 
 
-    bsp_board_leds_off();
-    bsp_board_led_on(0);
+    //bsp_board_leds_off();
+    //bsp_board_led_on(0);
     //m_buffer_tx[0]=0;
     //m_buffer_tx[1]=0;
     //m_buffer_tx[2]=0;
     //m_buffer_tx[3]=0;
-    memset(&m_buffer_tx, 0, sizeof(m_buffer_tx)); //try this to nuke the array.
     
-    while (nCount > 0) { 
+    //memset(&m_buffer_tx, 0, sizeof(m_buffer_tx)); //try this to nuke the array.
+    
+    /*while (nCount > 0) { 
         // storing remainder in binary array  
         m_buffer_tx[c] = nCount & 00000001; 
         nCount = nCount>>1;
         c++; 
-     }
+     }*/
 
      //err_code = nrf_drv_qspi_erase(NRF_QSPI_ERASE_LEN_64KB, 0);
      //APP_ERROR_CHECK(err_code);
      //nrf_drv_qspi_chip_erase();
      //WAIT_FOR_PERIPH();
 
-     err_code = nrf_drv_qspi_write(m_buffer_tx, sizeof(m_buffer_tx), LastKnownAddr);
+     err_code = nrf_drv_qspi_write(&TestHeader, sizeof(TestHeader), LastKnownAddr);
      APP_ERROR_CHECK(err_code);
      WAIT_FOR_PERIPH();
 
     if(err_code == NRF_SUCCESS) { //If write was success, then increment address (+1 so we start at a new place, no overlap)
-      LastKnownAddr += (sizeof(m_buffer_tx));
+      LastKnownAddr += (sizeof(TestHeader));
     }
  
      //bsp_board_leds_on();
-     pressCount = 0;
+     //pressCount = 0;
      lwrite_qspi = false;
 }
 
-void read_qspi()
+void read_qspi(uint32_t Address)
 {
-      bsp_board_leds_off();
+      //bsp_board_leds_off();
       ret_code_t err_code;
-      volatile uint8_t c = 0;
-      volatile uint8_t digit_2read = 0;
-      memset(&m_buffer_rx, 0, sizeof(m_buffer_rx)); //try this to nuke the array.
+      //volatile uint8_t c = 0;
+      //volatile uint8_t digit_2read = 0;
+      //memset(&m_buffer_rx, 0, sizeof(m_buffer_rx)); //try this to nuke the array.
 
-      err_code = nrf_drv_qspi_read(m_buffer_rx, sizeof(m_buffer_tx), (LastKnownAddr-sizeof(m_buffer_tx)));
+      err_code = nrf_drv_qspi_read(&ReadHeader, sizeof(ReadHeader), Address);
       APP_ERROR_CHECK(err_code);
       WAIT_FOR_PERIPH();
 
-      for (c = 0 ; c<4 ; ++c)
+      /*for (c = 0 ; c<4 ; ++c)
        {
           digit_2read = m_buffer_rx[3-c];
           if(digit_2read == 1)
           {
              bsp_board_led_on(c);
           }
-       } 
+       }*/
+        
        lread_qspi = false;
 }
--- bsi_qspi.h
+++ bsi_qspi.h
@@ -6,21 +6,26 @@ extern bool lwrite_qspi;
 extern bool lread_qspi;
 static volatile uint32_t LastKnownAddr = 0; // Attempt to track the most recent written memory block
 
-typedef struct Header {
+typedef struct BSI_Header {
       char BSI_Name[16];
       uint8_t StartTime[7]; //7 bytes (5+ 2byte year) YYYY/MM/DD/HH/MM/SS
-}Header;
+}BSI_Header;
 
-typedef struct Ad_gPacket {
-    char BSI_Name[16];
-    uint8_t StartTime[7]; //7 bytes (5+ 2byte year) YYYY/MM/DD/HH/MM/SS
-    //....
+typedef struct QSPI_Page {
+    uint32_t countMin; //minutes since Header StartTime[] (last Local Listener connection)
+    uint8_t sensorCh; //which sensor the following value is from (1=An1, 2=An2, or 9 = Pulse)
+    uint16_t sensorValue; //the 16b reading from the sensor (10-bit ADC for An# or Bool for Pulse)
+}QSPI_Page;
+
+typedef struct Advertisement_General_Packet {
+    struct BSI_Header Header;
+    struct QSPI_Page qPage[];
 }Ad_gPacket;
 
 void qspi_handler(nrf_drv_qspi_evt_t event, void * p_context);
 
 void configure_memory(void);
 
-void write_qspi(void);
+void write_qspi(uint32_t Address); //Address to write to
 
-void read_qspi(void);
+void read_qspi(uint32_t Address); //Address to read FROM

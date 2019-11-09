a1895e6c729e02217bb5ff2ffa29418291011980
Landon B <landon.brown26@online.camosun.com>
2019-11-08 17:02:44 -0800

    Revert "Update main.c"
    This reverts commit abf64338685b9a6b87a2cdbbf8f6aa06230b32ea.

--- main.c
+++ main.c
@@ -56,6 +56,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 #include "nordic_common.h"
 #include "nrf.h"
 #include "app_error.h"
@@ -89,6 +90,7 @@
 #include "sdk_config.h"
 #include "nrf_delay.h"
 #include "app_util_platform.h"
+
 
 #include "bsi_config.h"
 #include "bsi_qspi.h"
